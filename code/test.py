e298b12da7f51d0b55cc1825672177dfdd1cba79
Suraj Kumar Jana <surajjana2@gmail.com>
2016-01-16 16:41:48 +0530

    New code and data added @Suraj

--- code/test.py
+++ code/test.py
@@ -1,10 +1,45 @@
 import csv
+from gensim import corpora, models, similarities
 
 fv = []
+corpus = []
 i = 0
-with open("test1.tsv") as tsvfile:
+with open("train_data.tsv") as tsvfile:
     tsvreader = csv.reader(tsvfile, delimiter="\t")
     for line in tsvreader:
         fv.append(line[0:6])
 
-print fv[0][2]
+while(i<=1):
+	corpus.append(fv[i])
+	i = i + 1
+
+tpl0 = (0,)
+tpl1 = (1,)
+tpl2 = (2,)
+tpl3 = (3,)
+tpl4 = (4,)
+tpl5 = (5,)
+
+
+
+tpl0 += (fv[0][0],)
+tpl1 += (fv[0][1],)
+tpl2 += (fv[0][2],)
+tpl3 += (fv[0][3],)
+tpl4 += (fv[0][4],)
+tpl5 += (fv[0][5],)
+
+lst0 = []
+
+lst0.append(tpl0)
+lst0.append(tpl1)
+lst0.append(tpl2)
+lst0.append(tpl3)
+lst0.append(tpl4)
+lst0.append(tpl5)
+
+
+
+print lst0
+#tfidf = models.TfidfModel(corpus)
+#print tfidf
