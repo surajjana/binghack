a8822288b4fb4bc0ccf93689b20e87a73a7a9b5f
Suraj Kumar Jana <surajjana2@gmail.com>
2016-01-16 17:04:39 +0530

    Feature extraction code added @Suraj

--- code/test.py
+++ code/test.py
@@ -13,33 +13,36 @@ while(i<=1):
 	corpus.append(fv[i])
 	i = i + 1
 
-tpl0 = (0,)
-tpl1 = (1,)
-tpl2 = (2,)
-tpl3 = (3,)
-tpl4 = (4,)
-tpl5 = (5,)
+res_lst = []
+
+for i in range(0, 4498):
+	tpl0 = (0,)
+	tpl1 = (1,)
+	tpl2 = (2,)
+	tpl3 = (3,)
+	tpl4 = (4,)
+	tpl5 = (5,)
+	
+	tpl0 += (fv[i][0],)
+	tpl1 += (fv[i][1],)
+	tpl2 += (fv[i][2],)
+	tpl3 += (fv[i][3],)
+	tpl4 += (fv[i][4],)
+	tpl5 += (fv[i][5],)
+
+	lst0 = []
+
+	lst0.append(tpl0)
+	lst0.append(tpl1)
+	lst0.append(tpl2)
+	lst0.append(tpl3)
+	lst0.append(tpl4)
+	lst0.append(tpl5)
+
+	res_lst.append(lst0)
 
 
 
-tpl0 += (fv[0][0],)
-tpl1 += (fv[0][1],)
-tpl2 += (fv[0][2],)
-tpl3 += (fv[0][3],)
-tpl4 += (fv[0][4],)
-tpl5 += (fv[0][5],)
-
-lst0 = []
-
-lst0.append(tpl0)
-lst0.append(tpl1)
-lst0.append(tpl2)
-lst0.append(tpl3)
-lst0.append(tpl4)
-lst0.append(tpl5)
-
-
-
-print lst0
+print res_lst
 #tfidf = models.TfidfModel(corpus)
 #print tfidf
